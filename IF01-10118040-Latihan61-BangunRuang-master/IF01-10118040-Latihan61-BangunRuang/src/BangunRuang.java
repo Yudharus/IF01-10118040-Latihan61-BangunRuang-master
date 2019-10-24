dc37f8f84edd2a9a3e7b5b6457f0c41c2abafc33
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-10-24 14:46:41 +0700

    Add files via upload

---
+++ IF01-10118040-Latihan61-BangunRuang-master/IF01-10118040-Latihan61-BangunRuang/src/BangunRuang.java
@@ -0,0 +1,17 @@
+/*
+ * NAMA              : Moch Yudha Rusdian
+ * KELAS             : IF-01
+ * NIM               : 10118040
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk mencari volume beberapa bangun ruang      
+ */
+
+/**
+ *
+ * @author Fatahillah Seno
+ */
+public abstract class BangunRuang {
+
+    private double volume;
+
+    public abstract double hitungVolume();
+}
---
+++ IF01-10118040-Latihan61-BangunRuang-master/IF01-10118040-Latihan61-BangunRuang/src/Bola.java
@@ -0,0 +1,29 @@
+/*
+ * NAMA              : Moch Yudha Rusdian
+ * KELAS             : IF-01
+ * NIM               : 10118040
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk mencari volume beberapa bangun ruang    
+ */
+
+/**
+ *
+ * @author Fatahillah Seno
+ */
+public class Bola extends BangunRuang {
+
+    private int r;
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    @Override
+    public double hitungVolume() {
+        return (4 * Math.PI * r * r * r) / 3;
+    }
+
+}
